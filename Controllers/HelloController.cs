fda173fb9bed3155e1c264db962125c7212ff099
ldmachad <leonardodiomachado@gmail.com>
2023-10-13 14:03:25 -0300

    Hello ASP.NET

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace helloworld.Controllers
+{
+    [Route("~/")]
+    [ApiController]
+    public class HelloController
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello ASP.NET!";
+        }
+    }
+}
